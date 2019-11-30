cc61a329b4d4ef19c77dfe94f7786ca874b4fe96
rvaldizon <r.valdizon@gmail.com>
2019-11-30 17:09:20 -0500

    spark-session

--- src/depositClassifier.py
+++ src/depositClassifier.py
@@ -1,3 +1,16 @@
-from pyspark.ml.classification import gbtclassifier
+from pyspark.sql import SparkSession
+from pyspark import SparkConf
+from pyspark import SparkContext
+from pyspark.sql import SparkSession
+from pyspark.ml.classification import GBTClassifier
 from pyspark.ml import Pipeline
 from pyspark.ml.feature import OneHotEncoderEstimator, VectorAssembler, StringIndexer
+from pyspark.ml.evaluation import BinaryClassificationEvaluator
+
+conf =SparkConf().setAppName("bankClassifier").setMaster("local[*]")
+
+spark = SparkContext(conf = conf)
+
+#spark = SparkSession.builder.appName('abc').getOrCreate()
+data = spark.read.csv("../data/bank.csv", header = True, inferSchema = True)
+print(data.show())
